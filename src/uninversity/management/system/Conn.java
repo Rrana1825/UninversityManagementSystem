8a18c99ba40e4bcf4b4438b660700b2612c3061f
Rrana1825 <105504145+Rrana1825@users.noreply.github.com>
2024-02-21 18:42:19 +0530

    Add files via upload

---
+++ src/uninversity/management/system/Conn.java
@@ -0,0 +1,16 @@
+package uninversity.management.system;
+
+import java.sql.*;
+public class Conn {
+	Connection c;
+	Statement s;
+	Conn(){
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem","root","root");
+			s = c.createStatement();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
